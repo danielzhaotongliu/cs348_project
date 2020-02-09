42330f52a66d55690e581762e4f8c4196ec07c5d
Adil Mian <adilmian95@gmail.com>
2020-02-09 16:43:42 -0500

    added component to display a single Shoe

--- frontend/js/pages/Home.js
+++ frontend/js/pages/Home.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
+import ShoeComponent from '../app/example-app/components/ShoeComponent'
 
 const Home = () => {
   const [showBugComponent, setShowBugComponent] = useState(false);
 
   return (
     <>
-      <button type="button" onClick={() => setShowBugComponent(true)}>
+      {/* <button type="button" onClick={() => setShowBugComponent(true)}>
         Click to test if Sentry is capturing frontend errors! (Should only work in Production)
-      </button>
+      </button> */}
+
+      <ShoeComponent name={'Contigo'} price={20} brand={'Clarks'} size={10}/>
+
       {showBugComponent && showBugComponent.field.notexist}
     </>
   );
