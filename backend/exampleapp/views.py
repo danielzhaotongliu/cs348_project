b50a5ac322159f9ba2c8924054155707dd71954e
danielzhaotongliu <ztliu@edu.uwaterloo.ca>
2020-02-09 17:53:10 -0500

    WIP: working backend DRF view and endpoint for shoes

--- backend/exampleapp/views.py
+++ backend/exampleapp/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render  # noqa
+from django.shortcuts import render
+from rest_framework import viewsets
 
+from .models import Shoe
+from .serializers import ShoeSerializer
 
 # Create your views here.
+class ShoeViewSet(viewsets.ModelViewSet):
+    """
+    API endpoints to view shoe information
+    """
+    queryset = Shoe.objects.all()
+    serializer_class = ShoeSerializer
