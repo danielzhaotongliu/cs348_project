e24b6879e305dde75d3e165cb220f945677a608f
danielzhaotongliu <ztliu@edu.uwaterloo.ca>
2020-02-09 17:53:10 -0500

    WIP: working backend DRF view and endpoint for shoes

---
+++ backend/exampleapp/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+
+from .models import Shoe
+
+class ShoeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Shoe
+        fields = '__all__'
