679ef7087d54cba634d9c50c1b8509c01fc65b1c
danielzhaotongliu <ztliu@edu.uwaterloo.ca>
2020-02-11 18:45:15 -0500

    endpoint to search by brand: /api/shoe/?brand=your_brand

--- backend/exampleapp/views.py
+++ backend/exampleapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Shoe
 from .serializers import ShoeSerializer
@@ -9,5 +11,19 @@ class ShoeViewSet(viewsets.ModelViewSet):
     """
     API endpoints to view shoe information
     """
-    queryset = Shoe.objects.all()
     serializer_class = ShoeSerializer
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned shoes to a given user,
+        by filtering against different query parameters in the URL.
+        TODO: add case insensitive checking later
+        """
+        queryset = Shoe.objects.raw('SELECT * FROM exampleapp_shoe')
+        
+        # filter by brand if the parameter exists in the url
+        brand = self.request.query_params.get('brand', None)
+        if brand is not None:
+            queryset = Shoe.objects.raw('SELECT * FROM exampleapp_shoe WHERE brand = %s', [brand])
+        
+        return queryset
