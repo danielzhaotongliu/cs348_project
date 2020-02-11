c619e18039d694be33c86a47201753b22ff13037
MichaelTChau <michael@Michaels-Laptop.localdomain>
2020-02-11 16:24:28 -0500

    fix column headers

--- dataset/simplifyDataset.py
+++ dataset/simplifyDataset.py
@@ -2,16 +2,11 @@ import pandas as pd
 import uuid
 import random
 
-
-keepcolumns = ["id", "brand", "categories", "colors", "dateAdded", "descriptions", "imageURLs", "name", "prices.amountMin", "quantities", "sizes"]
-data = pd.read_csv("Datafiniti_Mens_Shoe_Prices.csv", nrows = 50) 
+#"dateAdded"
+keepcolumns = ["brand", "categories", "colors", "descriptions", "imageURLs", "name", "prices.amountMin", "quantities", "sizes"]
+data = pd.read_csv("Datafiniti_Mens_Shoe_Prices.csv", nrows = 20) 
 
 data = data[data.columns.intersection(keepcolumns)]
-
-
-#ID
-for k, v in data["id"].iteritems():
-    data["id"][k] = uuid.uuid4()
 
 #quantities
 for k, v in data["quantities"].iteritems():
@@ -36,5 +31,15 @@ for k, v in data["categories"].iteritems():
 #description
 for k, v in data["descriptions"].iteritems():
     data["descriptions"][k] = "Some descriptions"
+
+#name
+for k, v in data["name"].iteritems():
+    data["name"][k] = data["name"][k].replace(",","") 
+
+#imageURLs
+for k, v in data["imageURLs"].iteritems():
+    data["imageURLs"][k] = "www.google.com"
+
+data = data.rename(columns={'descriptions': 'description', 'quantities': 'stock', 'sizes': 'size', 'colors': 'colour', 'categories' : 'style', 'prices.amountMin' : 'price',  'imageURLs' : 'image_url'})
 
 data.to_csv("formatedData.csv", encoding='utf-8' , index = None, header=True) 
