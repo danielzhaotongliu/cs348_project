496b61f87b3323df320d657adcc89038a4b2a126
danielzhaotongliu <ztliu@edu.uwaterloo.ca>
2020-02-11 19:51:18 -0500

    hotfix: updated ALLOWED_HOSTS for deploy to production

--- backend/cs348_project/settings/base.py
+++ backend/cs348_project/settings/base.py
@@ -23,7 +23,7 @@ ADMINS = (
 
 AUTH_USER_MODEL = 'users.User'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".cs348shoeapp.herokuapp.com", "localhost", "127.0.0.1", "[::1]"]
 
 INSTALLED_APPS = [
     'exampleapp.apps.ExampleappConfig',
