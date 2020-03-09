e6f65b34b7807978e04a8c6a1212d1c34469686f
Adil Mian <adilmian95@gmail.com>
2020-03-08 20:56:38 -0400

    added skeleton of Cart page

---
+++ frontend/js/pages/CartPage.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+
+
+
+export default class CartPage extends React.Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            shoes : []
+        };
+    }
+
+
+    render(){
+        return (
+            <div>
+                <h1>Cart page</h1>
+            </div>
+        );
+    }
+
+};
+
+
+const styles = {
+
+};
